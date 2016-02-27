ed64884b0e71b51e3661cac424edb8f309b269bd
Peter Hultqvist <hultqvist@silentorbit.com>
2016-02-27 21:44:53 +0100

    Cleanup in attempts to get it running.
    The final trick was to edit project.json and set the value under the "commands" pair to the name of the project folder.

--- CodeGenerator/Properties/AssemblyInfo.cs
+++ CodeGenerator/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Peter Hultqvist 2015")]
+[assembly: AssemblyCopyright("Peter Hultqvist 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2015.10.13.*")]
+[assembly: AssemblyVersion("2016.2.28.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
